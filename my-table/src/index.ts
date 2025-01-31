d8ef4fe6129a5006d1ce2b942c35bafc5779a588
tigawanna <denniskinuthiawaweru@gmail.com>
2025-01-31 13:58:32 +0300

    refactor: remove unused files and implement DynamicRowsAndColumnsTable component for enhanced data rendering

--- my-table/src/index.ts
+++ my-table/src/index.ts
@@ -1,1 +1,9 @@
+export { DynamicRowsAndColumnsTable } from "./components/DynamicRowsAndColumnsTable";
+export { DynamicRowsTable } from "./components/DynamicRowsTable";
+export { GenericTable } from "./components/GenericTable";
+export { GenericTableWithNestedFields } from "./components/GenericTableWithNestedFields";
+export { HardCodedTable } from "./components/HardCodedTable";
+ 
+
+
 
