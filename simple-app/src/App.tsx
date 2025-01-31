e3493880d601227a942eb86d553262699791f377
tigawanna <denniskinuthiawaweru@gmail.com>
2025-01-31 10:22:02 +0300

    add GenericTable and GenericTableWithNestedFields components for dynamic data rendering

--- simple-app/src/App.tsx
+++ simple-app/src/App.tsx
@@ -1,6 +1,9 @@
 import { DynamicRowsAndColumnsTable } from "./components/DynamicRowsAndColumnsTable";
 import { DynamicRowsTable } from "./components/DynamicRowsTable";
+import { GenericTable } from "./components/GenericTable";
+import { GenericTableWithNestedFields } from "./components/GenericTableWithNestedFields";
 import { HardCodedTable } from "./components/HardCodedTable";
+import { playerranks, playerranksWithSatas } from "./data/paley-data";
 import viteLogo from "/vite.svg";
 
 function App() {
@@ -9,6 +12,26 @@ function App() {
       <div className="flex flex-col h-full items-center justify-center">
         <img src={viteLogo} className="h-52 w-52" alt="Vite logo" />
       </div>
+      <GenericTableWithNestedFields
+        data={playerranksWithSatas}
+        columns={[
+          { label: "ID", accessor: "id" },
+          { label: "Name", accessor: "name" },
+          { label: "Age", accessor: "age" },
+          { label: "Rank", accessor: "rank" },
+          { label: "wins", accessor: "stats.wins" },
+          { label: "losses", accessor: "stats.losses" },
+        ]}
+      />
+      <GenericTable
+        data={playerranks}
+        columns={[
+          { label: "ID", accessor: "id" },
+          { label: "Name", accessor: "name" },
+          { label: "Age", accessor: "age" },
+          { label: "Rank", accessor: "rank" },
+        ]}
+      />
       <DynamicRowsAndColumnsTable />
       <DynamicRowsTable />
       <HardCodedTable />
